694790a7ff7a5e9a103309a927c98d6b27b0f315
MACorman <madelinecorman@gmail.com>
2020-04-15 12:03:32 -0400

    did some react routes and can add foods to the diary

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import DiaryContainer from './DiaryContainer'
 import WeightContainer from './WeightContainer'
 import Logout from '../components/Logout'
+// import { Switch, Route, withRouter } from 'react-router-dom'
 
 class UserContainer extends React.Component {
     render() {
