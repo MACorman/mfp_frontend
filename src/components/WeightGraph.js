02aa9a6d530cd61b6edf74d862c91a18605d18ce
MACorman <madelinecorman@gmail.com>
2020-05-20 15:06:04 -0400

    can create account/new user. made some edits to the weight graph

--- src/components/WeightGraph.js
+++ src/components/WeightGraph.js
@@ -1,26 +1,18 @@
 import React from 'react'
-import {XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, ChartLabel } from 'react-vis'
-
-// const data = [
-//     {x: 0, y: 8},
-//     {x: 1, y: 5},
-//     {x: 2, y: 4},
-//     {x: 3, y: 9},
-//     {x: 4, y: 1},
-//     {x: 5, y: 7},
-//     {x: 6, y: 6},
-//     {x: 7, y: 3},
-//     {x: 8, y: 2},
-//     {x: 9, y: 0}
-//   ];
+import {XYPlot, LineMarkSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, ChartLabel } from 'react-vis'
 
 class WeightGraph extends React.Component {
 
     graphData = () => {
         let data = []
-        this.props.weights.map(weight => {
-            data.push({x: `${weight.date.slice(5, 7)}/${weight.date.slice(8, 10)}`, y: weight.measurement})
-        })
+        if(this.props.weights.length) {
+            this.props.weights.map(weight => {
+                data.push({x: `${weight.date.slice(5, 7)}/${weight.date.slice(8, 10)}`, y: weight.measurement})
+            })
+        } else {
+            let date = new Date().toISOString()
+            data = [{x: `${date.slice(5, 7)}/${date.slice(8, 10)}`, y: 0}]
+        }
         return data
     }
 
@@ -29,12 +21,12 @@ class WeightGraph extends React.Component {
         return(
             <div>
                 <p>Weight Graph</p>
-                <XYPlot height={300} width = {300} xType='ordinal'>
+                <XYPlot height={300} width = {300} xType='ordinal' yDomain={[40, 100]}>
                     <HorizontalGridLines style={{ stroke: "#e0e0e0"}}/>
                     <VerticalGridLines style={{ stroke: "#e0e0e0"}}/>
                     <XAxis />
                     <YAxis />
-                    <LineSeries style={{fill: 'none'}} data={this.graphData()}/>
+                    <LineMarkSeries style={{fill: 'none'}} markStyle={{fill: 'solid'}} data={this.graphData()}/>
                     <ChartLabel 
                         text="Date"
                         className="alt-x-label"
